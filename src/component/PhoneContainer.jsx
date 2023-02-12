e127f936aa725c866ec523ff6116fbde1808e494
Amirreza <amirreza.m13825@gmail.com>
2023-02-12 20:06:51 +0330

    get the global state in the component

--- src/component/PhoneContainer.jsx
+++ src/component/PhoneContainer.jsx
@@ -1,10 +1,27 @@
-const PhoneContainer = () => {
+import { connect } from "react-redux";
+import { buyPhone } from "../redux/phone/phoneActions";
+
+const PhoneContainer = ({ numOfPhones, buyPhone }) => {
   return (
     <div>
-      <h2>Number of Phones : </h2>
-      <button>Buy a Phone</button>
+      <h2>Number of Phones : {numOfPhones}</h2>
+      <button onClick={buyPhone}>Buy a Phone</button>
     </div>
   );
 };
 
-export default PhoneContainer;
+// Get the global state
+const mapStateToProps = (state) => {
+  return {
+    numOfPhones: state.numOfPhones,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyPhone: () => dispatch(buyPhone()),
+  };
+};
+
+// export
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneContainer);
