1772ad9e15904128f312c4770e4290895f8885f4
Amirreza <amirreza.m13825@gmail.com>
2023-02-12 20:12:30 +0330

    get the global state in the component in a better way with hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Provider } from "react-redux";
 import PhoneContainer from "./component/PhoneContainer";
 import store from "./redux/store";
+import HooksPhoneContainer from "./component/HooksPhoneContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <PhoneContainer />
+        <HooksPhoneContainer />
       </div>
     </Provider>
   );
---
+++ src/component/HooksPhoneContainer.jsx
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { buyPhone } from "../redux/phone/phoneActions";
+
+const HooksPhoneContainer = () => {
+  const numOfPhones = useSelector((state) => state.numOfPhones);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Number of Phones : {numOfPhones}</h2>
+      <button onClick={() => dispatch(buyPhone())}>Buy a Phone</button>
+    </div>
+  );
+};
+
+export default HooksPhoneContainer;
