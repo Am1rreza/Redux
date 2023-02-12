fb7eb41e3006057c1201b599187bb894d74febda
Amirreza <amirreza.m13825@gmail.com>
2023-02-12 22:48:19 +0330

    redux logger installed and added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+// Middleware
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
