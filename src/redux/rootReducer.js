99188524a75be8a642ea03a044cc368aeb1077d1
Amirreza <amirreza.m13825@gmail.com>
2023-03-09 21:23:29 +0330

    post reducer added to the root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import phoneReducer from "./phone/phoneReducer";
 import watchReducer from "./watch/watchReducer";
 import userReduser from "./user/userReducer";
+import postReducer from "./posts/postReducer";
 
 const rootReducer = combineReducers({
   phone: phoneReducer,
   watch: watchReducer,
   users: userReduser,
+  post: postReducer,
 });
 
 export default rootReducer;
