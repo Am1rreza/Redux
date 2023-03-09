69e3887b99536d09d3e15b2021cdfeda36e13d1a
Amirreza <amirreza.m13825@gmail.com>
2023-03-09 22:13:47 +0330

    rootSaga created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+import { rootSaga } from "./sagas/rootSaga";
 // Middleware
 import logger from "redux-logger";
 import thunk from "redux-thunk";
@@ -12,5 +13,8 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware))
 );
+
+// Then run the saga
+sagaMiddleware.run(rootSaga);
 
 export default store;
