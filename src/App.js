b1f80300fa10bb90801e4105288feda1c0aee236
Amirreza <amirreza.m13825@gmail.com>
2023-03-09 22:39:35 +0330

    PostSaga component completed (use post global state)

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import HooksPhoneContainer from "./component/PhoneContainer/HooksPhoneContainer"
 import HooksWatchContainer from "./component/watchContainer/watchContainer";
 import PhoneWithPayloadContainer from "./component/PhoneContainer/PhoneWithPayloadContainer";
 import UserContainer from "./component/UserContainer/UserContainer";
+import PostSaga from "./component/PostSaga/PostSaga";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <PhoneContainer />
+        {/* <PhoneContainer />
         <HooksPhoneContainer />
         <PhoneWithPayloadContainer />
         <HooksWatchContainer />
-        <UserContainer/>
+        <UserContainer/> */}
+        <PostSaga />
       </div>
     </Provider>
   );
--- src/component/PostSaga/PostSaga.jsx
+++ src/component/PostSaga/PostSaga.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPostRequest } from "../../redux/posts/postActions";
 
 const PostSaga = () => {
   const [postId, setPostId] = useState("");
+  const { loading, error, data } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -11,7 +15,21 @@ const PostSaga = () => {
         value={postId}
         onChange={(e) => setPostId(e.target.value)}
       />
-      <button>Get Post</button>
+      <button onClick={() => dispatch(fetchPostRequest(postId || 1))}>
+        Get Post
+      </button>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <p>{error}</p>
+      ) : data.title ? (
+        <div>
+          <p>post title: {data.title}</p>
+          <p>post body: {data.body}</p>
+        </div>
+      ) : (
+        <p>fetch some post !</p>
+      )}
     </>
   );
 };
